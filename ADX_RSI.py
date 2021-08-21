a9670877b954291b91b8d29e5247cad2e6b8c195
ketzah <53633920+ketzah@users.noreply.github.com>
2021-08-21 18:55:35 -0500

    Update ADX_RSI.py
    Added hyperopt buy space
    Disabled sell trend (relying on ROI)

--- ADX_RSI.py
+++ ADX_RSI.py
@@ -18,22 +18,24 @@ logger = logging.getLogger(__name__)
 ###
 
 class ADX_RSI(IStrategy):
-    
+    buy_rsi = IntParameter(10, 30, default=29, space="buy") 
+    buy_adx = IntParameter(25, 40, default=39, space="buy")
+    #sell_rsi = IntParameter(70, 95, default=70, space="sell")
       
    
+    #105/2000:   1244 trades. 741/494/9 Wins/Draws/Losses. Avg profit   0.68%. Median profit   0.22%. ((225 days))
+    #Total profit  12492.51785256 USD ( 1249.25%). Avg duration 7:42:00 min. Objective: -22.71919
 
     # ROI table:
     minimal_roi = {
-        #"0": 0.10,
-        #"20": 0.06,
-        #"40": 0.03,
-        #"60": 0.01,
-        #"120": 0.00
-        "0": 10
+        "0": 0.154,
+        "11": 0.08,
+        "34": 0.026,
+        "80": 0
     }
 
     # Stoploss:
-    stoploss = -0.99
+    stoploss = -0.346
    
     # Trailing stop:
     trailing_stop = False
@@ -62,10 +64,10 @@ class ADX_RSI(IStrategy):
     process_only_new_candles = False
     
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        dataframe['rsi'] = ta.RSI(dataframe, timeperiod=12) # overbought and oversold conditions
-        dataframe['adx'] = ta.ADX(dataframe, timeperiod=12) #trend detection
-        dataframe['sma12'] = ta.SMA(dataframe, timeperiod=12) #guard
-        dataframe['sma50'] = ta.SMA(dataframe, timeperiod=50) #guard
+        dataframe['rsi'] = ta.RSI(dataframe, timeperiod=14) # overbought and oversold conditions
+        dataframe['adx'] = ta.ADX(dataframe, timeperiod=14) #trend detection
+        #dataframe['sma12'] = ta.SMA(dataframe, timeperiod=12) #guard
+        #dataframe['sma50'] = ta.SMA(dataframe, timeperiod=50) #guard
     
         return dataframe
 
@@ -73,22 +75,22 @@ class ADX_RSI(IStrategy):
               
         dataframe.loc[
             (
-                ((dataframe["rsi"] <= 30)) & #oversold condition
-                ((dataframe["adx"] >= 25)) & #verify trend
-                ((dataframe["sma12"]) < (dataframe["sma50"])) & #guard
+                ((dataframe["rsi"] <= self.buy_rsi.value)) & #oversold condition
+                ((dataframe["adx"] >= self.buy_adx.value)) & #verify trend
+                #((dataframe["sma12"]) < (dataframe["sma50"])) & #guard
                 (dataframe['volume'] > 0) # volume above zero
             )
         ,'buy'] = 1
         return dataframe
 
     def populate_sell_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        
+        #disabled
         dataframe.loc[
             (
-                ((dataframe["rsi"] >= 70)) & #overbought condition
-                ((dataframe["adx"] >= 25)) & #verify trend
-                ((dataframe["sma12"]) > (dataframe["sma50"])) & #guard
+                #((dataframe["rsi"] >= self.sell_rsi.value)) & #overbought condition
+                #((dataframe["adx"] >= 25)) & #verify trend
+                #((dataframe["sma12"]) > (dataframe["sma50"])) & #guard
                 (dataframe['volume'] > 0) # volume above zero
             )
-        ,'sell'] = 1
+        ,'sell'] = 0
         return dataframe
